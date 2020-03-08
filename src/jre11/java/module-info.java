d5ce10e09fe38be2880b27e1da88ed52a6efe8c9
GedMarc <ged_marc@hotmial.com>
2020-03-08 16:59:28 +0200

    Remove Jar syntax as JDK 8 is no longer supported

--- src/jre11/java/module-info.java
+++ src/jre11/java/module-info.java
@@ -9,7 +9,6 @@ module com.jwebmp.plugins.angularprogressbuttonstyles {
 	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.angularprogressbuttonstyles.AngularProgressButtonStylesPageConfigurator;
 	provides com.jwebmp.core.base.angular.services.IAngularModule with com.jwebmp.plugins.angularprogressbuttonstyles.AngularProgressButtonStylesModule;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions with com.jwebmp.plugins.angularprogressbuttonstyles.implementations.AngularProgressStyleButtonsExclusionsModule;
 	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions with com.jwebmp.plugins.angularprogressbuttonstyles.implementations.AngularProgressStyleButtonsExclusionsModule;
 
 	opens com.jwebmp.plugins.angularprogressbuttonstyles to com.fasterxml.jackson.databind, com.jwebmp.core;
--- src/main/java/com/jwebmp/plugins/angularprogressbuttonstyles/implementations/AngularProgressStyleButtonsExclusionsModule.java
+++ src/main/java/com/jwebmp/plugins/angularprogressbuttonstyles/implementations/AngularProgressStyleButtonsExclusionsModule.java
@@ -1,6 +1,5 @@
 package com.jwebmp.plugins.angularprogressbuttonstyles.implementations;
 
-import com.guicedee.guicedinjection.interfaces.IGuiceScanJarExclusions;
 import com.guicedee.guicedinjection.interfaces.IGuiceScanModuleExclusions;
 
 import javax.validation.constraints.NotNull;
@@ -8,18 +7,8 @@ import java.util.HashSet;
 import java.util.Set;
 
 public class AngularProgressStyleButtonsExclusionsModule
-		implements IGuiceScanModuleExclusions<AngularProgressStyleButtonsExclusionsModule>,
-				           IGuiceScanJarExclusions<AngularProgressStyleButtonsExclusionsModule>
+		implements IGuiceScanModuleExclusions<AngularProgressStyleButtonsExclusionsModule>
 {
-
-	@Override
-	public @NotNull Set<String> excludeJars()
-	{
-		Set<String> strings = new HashSet<>();
-		strings.add("jwebmp-angular-progress-button-styles-*");
-		return strings;
-	}
-
 	@Override
 	public @NotNull Set<String> excludeModules()
 	{
